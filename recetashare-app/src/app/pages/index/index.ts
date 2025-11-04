9416f2c86f46a8f4f7e18107cd5f5f972cc4e543
gmbarrera <gmbarrera@gmail.com>
2025-11-04 20:02:14 +0100

    index

--- recetashare-app/src/app/pages/index/index.ts
+++ recetashare-app/src/app/pages/index/index.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   imports: [],
   templateUrl: './index.html',
-  styleUrl: './index.css'
+  styleUrls: ['./index.css']
 })
-export class Index {
+export class Index implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.inicializar()
+  }
+
+  inicializar() {
+    const botonTema = document.getElementById("boton-tema");
+    if (!botonTema) {
+      console.warn('boton-tema no encontrado en el DOM');
+      return;
+    }
+    const cuerpo = document.documentElement;
+
+    botonTema.addEventListener("click", () => {
+      const temaActual = cuerpo.getAttribute("data-tema") || "claro";
+      cuerpo.setAttribute("data-tema", temaActual === "claro" ? "oscuro" : "claro");
+    });
+  }
 }
