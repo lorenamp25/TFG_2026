f368dc1c1b95b914bc6c2e620b16de9262bebd31
gmbarrera <gmbarrera@gmail.com>
2025-11-10 19:00:42 +0100

    Fix login

--- recetashare-app/src/app/pages/login/login.ts
+++ recetashare-app/src/app/pages/login/login.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class LoginPage {
+  form = new FormGroup({
+    id: new FormControl(0)
+  })
+ onLogin() {
 
-
+  }
 
 }
