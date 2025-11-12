a94bd040c5535638d10d986fe9289a5a9456c2aa
Lorena <lorena.marpin@educa.jcyl.es>
2025-11-12 19:02:30 +0100

    css

---
+++ recetashare-app/src/app/cardcategoria/cardcategoria.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardcategoria',
+  imports: [],
+  templateUrl: './cardcategoria.html',
+  styleUrl: './cardcategoria.css',
+})
+export class Cardcategoria {
+
+}
